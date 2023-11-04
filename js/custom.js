6d7ef72a0ce62c586600bf9c643d33c54d7c82a0
minhazul2022 <145312173+minhazul2022@users.noreply.github.com>
2023-11-04 18:33:33 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+
+// client section owl carousel
+$(".client_owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    nav: true,
+    navText: [],
+    autoplay: true,
+    autoplayHoverPause: true,
+    navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+});
+
+var sticky = document.querySelector('.sticky');
+
+if (sticky.style.position !== 'sticky') {
+  var stickyTop = sticky.offsetTop;
+
+  document.addEventListener('scroll', function () {
+    window.scrollY >= stickyTop ?
+      sticky.classList.add('fixed') :
+      sticky.classList.remove('fixed');
+  });
+}
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+const stickyNavbar = document.querySelector("header.sticky-navbar");
+
+document.addEventListener("scroll", () => {
+
+    if (window.scrollY > 36) {
+        stickyNavbar.classList.add("scrolling");
+    } else {
+        stickyNavbar.classList.remove("scrolling");
+    }
+
+})
